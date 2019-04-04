7296419fbef1559400470a656091970903e25c58
1141813064 <1141813064@qq.com>
2019-04-04 16:50:04 +0800

    First Commit

---
+++ 1141813064/四则运算/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Random x = new Random();
+            for (int i = 1; i < n; i++)
+            {
+                double a = x.Next(0, 100);
+                double b = x.Next(0, 100);
+                double c = x.Next(0, 100);
+                double result = 0;
+                int d = x.Next(1, 6);
+                switch (d)
+                {
+                    case 1:
+                        result = a * b + c;
+                        if (result > 0)
+                            Console.WriteLine(a + "*" + b + "+" + c + "=");
+                        break;
+                    case 2:
+                        result = a + b + c;
+                        if (result > 0)
+                            Console.WriteLine(a + "+" + b + "+" + c + "=");
+                        break;
+                    case 3:
+                        result = a / b - c;
+                        if (result > 0)
+                            Console.WriteLine(a + "/" + b + "-" + c + "=");
+                        break;
+                    case 4:
+                        result = a + b - c;
+                        if (result > 0)
+                            Console.WriteLine(a + "+" + b + "-" + c + "=");
+                        break;
+                    case 5:
+                        result = a + b * c;
+                        if (result > 0)
+                            Console.WriteLine(a + "+" + b + "*" + c + "=");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
